61d3dc5d91c49f07120c3f83f542a90b70c1cb21
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-08-19 14:14:10 -0400

    haha

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -85,9 +85,9 @@ const AboutSection = () => {
             <Image
               src="/hero.jpeg"
               alt=""
-              width={325}
-              height={325}
-              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
+              width={250}
+              height={250}
+              className="hidden md:block md:relative md:bottom md:left-32 md:z-0"
             />
           </div>
         </div>
