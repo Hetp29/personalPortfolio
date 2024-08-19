a83c3d383c373ea997a278a0bc26fed827a8a004
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-08-19 10:26:48 -0400

    Completed AboutMe

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -2,17 +2,24 @@ import React from "react"
 import Image from "next/image"
 
 const skills = [
-  { skill: "HTML" },
-  { skill: "CSS" },
+  { skill: "Java" },
+  { skill: "Python" },
   { skill: "JavaScript" },
   { skill: "TypeScript" },
-  { skill: "Python" },
-  { skill: "React" },
+  { skill: "HTML/CSS" },
+  { skill: "React.js" },
+  { skill: "Node.js/Express" },
+  { skill: "Express.js" },
   { skill: "Next.js" },
+  { skill: "Django" },
   { skill: "Tailwind CSS" },
+  { skill: "Chakra UI" },
+  { skill: "Material UI" },
+  { skill: "MongoDB" },
+  { skill: "PostgreSQL" },
+  { skill: "Firebase" },
   { skill: "Git" },
   { skill: "GitHub" },
-  { skill: "Jupyter Notebooks" },
 ]
 
 const AboutSection = () => {
@@ -30,24 +37,24 @@ const AboutSection = () => {
               Get to know me!
             </h1>
             <p>
-              Hi, my name is Hosna and I am a{" "}
-              <span className="font-bold">{"highly ambitious"}</span>,
-              <span className="font-bold">{" self-motivated"}</span>, and
-              <span className="font-bold">{" driven"}</span> software engineer
-              based in Los Angeles, CA.
+              Hi, my name is Het and I am a{" "}
+              Computer Science/Data
+              Science student based in Monroe Township, NJ.
             </p>
             <br />
             <p>
-              I graduated from California State University, Northridge in 2019
-              with a BS in Computer Engineering and have been working in the
-              field ever since.
+            I expect to graduate from Rutgers University-New Brunswick in
+              December 2026 with a Bachelors of Science in Computer Science and Data Science. I
+              aspire to explore the realms of Software Engineering.
             </p>
             <br />
             <p>
-              I have a wide range of hobbies and passions that keep me busy.
-              From reading, playing sports, traveling, to making YouTube videos,
-              I am always seeking new experiences and love to keep myself
-              engaged and learning new things.
+            I believe that you should{" "}
+              <span className="font-bold text-teal-500">never stop growing</span>{" "}
+              and that&#39;s what I strive to do. I have a passion for
+              technology and a desire to always push the limits of what is
+              possible. I am excited to see where my career takes me and am
+              always open to new opportunities.
             </p>
             <br />
             <p>
@@ -58,7 +65,7 @@ const AboutSection = () => {
               and that&#39;s what I strive to do, I have a passion for
               technology and a desire to always push the limits of what is
               possible. I am excited to see where my career takes me and am
-              always open to new opportunities. 🙂
+              always open to new opportunities. 
             </p>
           </div>
           <div className="text-center md:w-1/2 md:text-left">
@@ -76,7 +83,7 @@ const AboutSection = () => {
               })}
             </div>
             <Image
-              src="/hero-image.png"
+              src="/hero.jpeg"
               alt=""
               width={325}
               height={325}
