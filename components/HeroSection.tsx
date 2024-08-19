70b5db73255d4b347b664efa357d9bd9c37a00ea
Hetp29 <143897109+Hetp29@users.noreply.github.com>
2024-08-19 10:18:10 -0400

    HeroSection complete

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -20,16 +20,20 @@ const HeroSection = () => {
         <div className="md:mt-2 md:w-3/5">
           <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Het!</h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
-            I&#39;m a{" "}
-            <span className="font-semibold text-teal-600">
-              Software Engineer{" "}
+            I&#39;m a student at{" "}
+            <span className="font-semibold text-red-600">
+              Rutgers University-New Brunswick{" "}
             </span>
-            based in Los Angeles, CA. Working towards creating software that
-            makes life easier and more meaningful.
+            pursuing a bachelors of science in Computer Science and Data Science
+            looking to graduate in December 2026. I am interested in the fields
+            of software engineering, software development, full stack
+            development, data science, and data analysis, leveraging my
+            background in Computer Science and Data Science.
           </p>
+          <div className="flex flex-col md:flex-row md:space-x-4">
           <Link
             to="projects"
-            className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
+            className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700 mb-4"
             activeClass="active"
             spy={true}
             smooth={true}
@@ -38,6 +42,18 @@ const HeroSection = () => {
           >
             Projects
           </Link>
+          <Link
+            to="Experience"
+            className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700 mb-4"
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Experience
+          </Link>
+          </div>
         </div>
       </div>
       <div className="flex flex-row items-center text-center justify-center ">
